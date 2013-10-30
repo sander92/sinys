286cb5491cd1fab72201b18be2874df71a929dba
Maanus A <maanus.av@gmail.com>
2013-10-30 15:33:49 +0200

    Initial history tab

--- trunk/POS/src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
+++ trunk/POS/src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
@@ -1,8 +1,18 @@
 package ee.ut.math.tvt.salessystem.ui.tabs;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
 
 /**
  * Encapsulates everything that has to do with the purchase tab (the tab
@@ -13,10 +23,63 @@ public class HistoryTab {
     // TODO - implement!
 
     public HistoryTab() {} 
-    
-    public Component draw() {
-        JPanel panel = new JPanel();
-        // TODO - Sales history tabel
-        return panel;
-    }
+
+
+
+	  private SalesSystemModel model;
+
+	  public HistoryTab(SalesSystemModel model) {
+	    this.model = model;
+	  }
+
+
+	  public Component draw() {
+	    JPanel panel = new JPanel();
+	    panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+	    GridBagLayout gb = new GridBagLayout();
+	    GridBagConstraints gc = new GridBagConstraints();
+	    panel.setLayout(gb);
+
+	    gc.fill = GridBagConstraints.HORIZONTAL;
+	    gc.anchor = GridBagConstraints.NORTH;
+	    gc.gridwidth = GridBagConstraints.REMAINDER;
+	    gc.weightx = 1.0d;
+	    gc.weighty = 0d;
+
+
+
+	    gc.weighty = 1.0;
+	    gc.fill = GridBagConstraints.BOTH;
+	    panel.add(drawHistoryMainPane(), gc);
+	    return panel;
+	  }
+
+
+
+
+
+	  private Component drawHistoryMainPane() {
+	    JPanel panel = new JPanel();
+
+	    JTable table = new JTable();
+
+	    JTableHeader header = table.getTableHeader();
+	    header.setReorderingAllowed(false);
+
+	    JScrollPane scrollPane = new JScrollPane(table);
+
+	    GridBagConstraints gc = new GridBagConstraints();
+	    GridBagLayout gb = new GridBagLayout();
+	    gc.fill = GridBagConstraints.BOTH;
+	    gc.weightx = 1.0;
+	    gc.weighty = 1.0;
+
+	    panel.setLayout(gb);
+	    panel.add(scrollPane, gc);
+
+	    panel.setBorder(BorderFactory.createTitledBorder("Order history:"));
+	    return panel;
+	  }
+
 }
